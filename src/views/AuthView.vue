14ff752ee9d319041941bdf908b65c2332f191d9
Matias Cipollone <107125293+MatiasCipollone@users.noreply.github.com>
2024-08-23 01:56:31 -0300

    first commit with template

---
+++ src/views/AuthView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <form>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input
+          type="email"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          placeholder="Enter email"
+          v-model="email"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input
+          type="password"
+          class="form-control"
+          placeholder="Password"
+          id="exampleInputPassword1"
+          v-model="password"
+        />
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, Ref } from "vue";
+
+let email: Ref<string> = ref("");
+let password: Ref<string> = ref("");
+</script>
+
+<style scoped></style>
